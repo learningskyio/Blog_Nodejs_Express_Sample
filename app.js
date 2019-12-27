eef51a0626c2e9d0d0c398e342590066629fe656
ArcherHuang <mmosconii@gmail.com>
2019-12-27 10:27:25 +0800

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+// Run: nodemon app.js
+
+// 引入套件
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// 使用 express 與設定 port 為 3000
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// home 路由
+app.use(require('./routes'))
+
+// 啟動伺服器
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
